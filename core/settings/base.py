1b30de93310bba7d9962f20b35e476ece1050b64
tmalik258 <talhamalik25.tm@gmail.com>
2024-06-11 20:23:04 +0500

    Moved static and templates directory to main dir

--- core/settings/base.py
+++ core/settings/base.py
@@ -106,11 +106,12 @@ USE_TZ = True
 
 
 # Static files and Media files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = [BASE_DIR / 'store/static']
-# STATICFILES_DIR = [BASE_DIR / "static"]
+# STATIC_ROOT = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+	BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
