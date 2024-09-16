bb3377d23965016bf00271da1d4a78290519030c
tmalik258 <talhamalik25.tm@gmail.com>
2024-09-16 16:45:23 +0500

    updated requirements.txt and prod.py

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -3,13 +3,27 @@ from .base import *
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_DATABASE"),
+	# Postgresql
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.environ.get("DB_DATABASE"),
+    #     'USER': os.environ.get("DB_USER"),
+    #     'PASSWORD': os.environ.get("DB_PASSWORD"),
+    #     'HOST': os.environ.get("DB_HOST"),
+    #     'PORT': os.environ.get("DB_PORT", 5432),
+    # }
+
+	'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT", 5432),
+        'PORT': os.environ.get("DB_PORT"),
+        'OPTIONS': {
+            'unix_socket': '/var/lib/mysql/mysql.sock',  # SHOW VARIABLES LIKE 'socket';
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
