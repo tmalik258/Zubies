e1f25c9da4ad112f3ede18d6f71423966c694afc
tmalik258 <talhamalik25.tm@gmail.com>
2024-09-20 10:27:18 +0500

    updated storages.py to fix issue

--- core/storages.py
+++ core/storages.py
@@ -1,21 +1,15 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+from django.contrib.staticfiles.storage import ManifestFilesMixin
 
 class MediaStore(S3Boto3Storage):
     location = 'media'
     file_overwrite = True
 
-class StaticManifestS3Storage(ManifestStaticFilesStorage, S3Boto3Storage):
+class StaticManifestS3Storage(ManifestFilesMixin, S3Boto3Storage):
     location = 'static'
 
     def __init__(self, *args, **kwargs):
-        # Remove 'storage' from kwargs if present
-        kwargs.pop('storage', None)
-        S3Boto3Storage.__init__(self, *args, **kwargs)
-        ManifestStaticFilesStorage.__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def hashed_name(self, name, content=None, filename=None):
-        return ManifestStaticFilesStorage.hashed_name(self, name, content, filename)
-
-    def url(self, name, parameters=None, expire=None):
-        return S3Boto3Storage.url(self, name, parameters=parameters, expire=expire)
+        return super().hashed_name(name, content, filename)
