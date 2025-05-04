8ffde8b9563025e7e83430abaf1105fc71ea9d0c
Talha Malik <talhamalik25.tm@gmail.com>
2025-05-04 21:15:10 +0500

    changed dependencies

--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 set -e
 
-# Check if the certificate exists, if not, generate a self-signed one
-# if [ ! -f /etc/letsencrypt/live/zubies.com/fullchain.pem ]; then
-#     echo "No SSL certificate found, creating self-signed certificate"
-#     mkdir -p /etc/letsencrypt/live/zubies.com/
-#     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-#         -keyout /etc/letsencrypt/live/zubies.com/privkey.pem \
-#         -out /etc/letsencrypt/live/zubies.com/fullchain.pem \
-#         -subj "/CN=zubies.com"
-    
-#     # Set permissions so nginx can read these files
-#     chmod -R 755 /etc/letsencrypt/live/
-# fi
+# Wait for certificates
+while [ ! -f /etc/letsencrypt/live/zubies.com/fullchain.pem ]; do
+  echo "Waiting for certificates..."
+  sleep 5
+done
 
 # Start nginx
 exec nginx -g "daemon off;"
